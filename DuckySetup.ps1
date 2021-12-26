ae868bc6456a1aea15d0599d1533ff778158a993
Mircox <mircea.rautoiu@gmail.com>
2021-12-26 23:36:25 +0200

    Add files

---
+++ DuckySetup.ps1
@@ -0,0 +1,39 @@
+#check for Raspberry Pi Pico:
+try{$drive = (Get-Volume -FriendlyName RPI-RP2 -erroraction stop)}
+catch{
+  "Couldn't find Ducky candidate!"
+  Read-Host -Prompt "`r`nPress Enter to exit"
+  Exit
+}
+
+$dLetter = $drive.DriveLetter
+Write-Host "Ducky candidate found, Starting AutoSetup:"
+$root = $dLetter + ":\"
+
+#copy circuitpython.uf2 to ducky:
+Write-Host "Copying circuitpython.uf2..."
+Copy-Item -Path adafruit-circuitpython-raspberry_pi_pico-en_US-7.0.0.uf2 -Destination $root
+Write-Host "Waiting for Ducky to reboot..."
+$drive = $null
+Do{
+  Start-Sleep -Milliseconds 500
+  try{$drive = (Get-Volume -FriendlyName CIRCUITPY -erroraction stop)}
+  catch{}
+}While($drive -eq $null)
+
+Write-Host "Ducky is up!"
+
+$dLetter = $drive.DriveLetter
+$root = $dLetter + ":\"
+
+#copy hid folder:
+Write-Host "Copying hid library..."
+Copy-Item -Path adafruit_hid -Destination ($root + "lib\adafruit_hid")
+
+#copy code.py (duckyinpython.py):
+Write-Host "Copying code.py duckyinpython.py..."
+Copy-Item -Path code.py -Destination ($root + "code.py")
+
+Write-Host "ALMOST DONE! Copy your payload.dd file to start hacking!"
+
+Read-Host -Prompt "`r`nPress Enter to exit"
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 DuckyAutoSetup
+A PowerShell script for automating the process of setting up a Raspberry PI Pico with circuitpython to turn it into a Rubber Ducky/Bad USB
+
+How to use?
+Just download the ZIP, extract it and run DuckySetup.ps1
+
+Here are some useful links:
+1. A list of Payloads: https://github.com/hak5darren/USB-Rubber-Ducky/wiki/Payloads
+2. Youtube tutorial for using the rubber ducky: https://youtu.be/e_f9p-_JWZw
